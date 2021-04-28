9323dc861c04f30fefacdb36d9e16d412857b8cc
Konstantin Alpashkin <konstantin.alpashkin@gmail.com>
2021-04-28 12:52:35 +0300

    work on site map parsing

---
+++ src/main/java/com/consalpa/Main.java
@@ -0,0 +1,15 @@
+package com.consalpa;
+
+import com.consalpa.Workers.Scrapper;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(new Scrapper());
+
+    }
+}
